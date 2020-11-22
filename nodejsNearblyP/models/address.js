33f2cd4cc0b43b44c9c10236c446c29dde1777c3
sari shteren <sh6197174@gmail.com>
2020-11-23 00:39:59 +0200

    css

--- nodejsNearblyP/models/address.js
+++ nodejsNearblyP/models/address.js
@@ -8,62 +8,27 @@ const Address = database.db.define('NBCTest', {
         defaultValue: Sequelize.UUIDV1,
         primaryKey: true,
     },
-    search_place: {
+    search_string: {
         type: Sequelize.STRING,
         allowNull: false,
     },
-    result_place1: {
+    name_place: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    address_place: {
         type: Sequelize.STRING,
         allowNull: true,
     },
-    result_name1: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_place2: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_name2: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_place3: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_name3: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_place4: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_name4: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_place5: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    result_name5: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
+
 });
-database.db.sync({ force: true })
-    .then(() => Address.create({
-        search_place: "בית קפה בבני ברק",
-        result_place1: "רבי עקיבא 80",
-        result_name1: "קפה פרידמן",
-        result_place2: "רבי עקיבא 85",
-        result_name2: "cofix",
-        result_place3: "בן גוריון 30",
-        result_name3: "caffe caffe",
-    }).then((user) => {
-        console.log(user);
-    }));
+// database.db.sync({ force: true })
+//     .then(() => Address.create({
+//         search_string: "פיצה רבי עקיבא 68 בני ברק",
+//         name_place: "פיצה מקס",
+//         address_place: "רבי עקיבא 68",
+//     }).then((user) => {
+//         console.log(user);
+//     }));
 
 module.exports = Address;
--- nodejsNearblyP/searchPlaces.js
+++ nodejsNearblyP/searchPlaces.js
@@ -20,18 +20,25 @@ router.get('/all', function (req, res) {
 
 router.get('/:mySearchP', function (req, res) {
     myQuery = "";
+    debugger;
     myQuery = req.params["mySearchP"].split(" ").join("+");
     const encodedURI = encodeURI(myQuery)
     console.log("mySearchP");
     request(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodedURI}&radius=12&key=AIzaSyBxvqGxEvb6ZBnyRTM8isBU_6O-MAfuNiQ&language =iw`
         , function (error, response, body) {
             if (body != null) {
+                jsonBody = JSON.parse(body);
+                //save in db
+
+                // for (var adress of jsonBody.results) {
+                //     db.add(adress.place, adress.name, adress.formatted_address);
+                // }
                 res.send(body);
             }
             else {
                 res.send("error!");
             }
-            // fs.writeFile('response.json', body);
+
 
             console.error('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -39,50 +46,5 @@ router.get('/:mySearchP', function (req, res) {
         });
 
 })
-
-// router.get('/:mySearchP', function (req, res) {
-//     console.log("mySearchP")
-//     request('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBxvqGxEvb6ZBnyRTM8isBU_6O-MAfuNiQ'
-//         , function (error, response, body) {
-//             console.error('error:', error); // Print the error if one occurred
-//             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//             console.log('body:', body); // Print the HTML for the Google homepage.
-//         });
-
-// })
-
-// router.get('/:mySearchP', function (req, res) {
-//     console.log("mySearchP")
-//     request('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key= AIzaSyBxvqGxEvb6ZBnyRTM8isBU_6O-MAfuNiQ'
-//         , function (error, response, body) {
-//             console.error('error:', error); // Print the error if one occurred
-//             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//             console.log('body:', body); // Print the HTML for the Google homepage.
-//         });
-
-// })
-
-// router.get('/:mySearchP', function (req, res) {
-//     console.log("mySearchP")
-
-//     try {
-//         const response = Places.nearbysearch({
-//             location: "-37.814,144.96332", // LatLon delimited by ,
-//             // radius: "500",  // Radius cannot be used if rankBy set to DISTANCE
-//             type: [], // Undefined type will return all types
-//             rankby: "distance" // See google docs for different possible values
-//         });
-//         res.send(response);
-//         const { status, results, next_page_token, html_attributions } = response;
-//     } catch (error) {
-//         console.log(error);
-//     }
-//     debugger;
-
-// })
-
-
-
-
 
 module.exports = router;
